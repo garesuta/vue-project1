8fa58fde7ecb56ad2f5094b16162ac8f71bc8fe7
garesuta <garesuta@gmail.com>
2025-01-08 23:42:47 +0700

    add new config for deploying on vercel

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,23 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
-    // Load environment variables based on the current mode (e.g., development or production)
     const env = loadEnv(mode, process.cwd());
 
     return {
         plugins: [vue()],
+        build: {
+            outDir: 'dist',
+        },
         server: {
-            proxy: {
+            proxy: mode === 'development' ? {
                 '/api': {
-                    target: env.VITE_API_HOST, // Use the loaded environment variable
+                    target: env.VITE_API_HOST,
                     changeOrigin: true,
                     rewrite: (path) => path.replace(/^\/api/, ''),
                 },
-            },
+            } : undefined,
         },
     };
 });
+
 
